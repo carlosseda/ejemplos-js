ed27b3e9a649c82018a0dcf2680118ba20156a50
carlosseda <carlossedagambin@gmail.com>
2022-04-27 15:02:36 +0200

    Refactor

--- Eventos/main.js
+++ Eventos/main.js
@@ -8,14 +8,15 @@ let square = document.getElementById("square");
 
 // En la siguiente linea se establece un evento que se ejecutara cada vez
 // que se presione el elemento de la variable square (es decir, el elemento
-// HTML que tiene una id "square").
+// HTML que tiene una id "square"). Aquí una lista de los eventos disponibles
+// en javascript: https://www.w3schools.com/jsref/dom_obj_event.asp
 square.addEventListener("click", () => {
 
     // En la siguiente línea se añade una clase "active" al elemento HTML que tiene
     // una id "square". 
-    square.classList.add("active"); 
+    // square.classList.add("active"); 
 
     // En la siguiente línea se añade una clase "active" al elemento HTML que tiene
     // una id "square" si no tiene la clase, y se la quita si la tiene.  
-    // square.classList.toggle("active"); 
+    square.classList.toggle("active"); 
 });
