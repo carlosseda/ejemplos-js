1f8e50cdbf4d16815839274e6c8fd6dad729b816
carlosseda <carlossedagambin@gmail.com>
2022-04-29 08:25:19 +0200

    Módulos

---
+++ Modulos/app.js
@@ -0,0 +1,10 @@
+// Para poder utilizar un módulo externo debemos importarlo en nuestro
+// archivo principal. En este caso, el archivo principal es "app.js".
+// En nodejs podemos utilizar la palabra "require" en vez 
+// de "import" para importar un módulo. Entre las llaves debemos
+// escribir el nombre del módulo que queremos importar. Finalmennte utilizamos
+// "from" para indicar el nombre del archivo que contiene el módulo. 
+
+import {renderTabs} from './tabs.js';
+
+renderTabs();
